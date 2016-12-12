7914c8de92d785ff210764ab746cd84118916db7
Andreas Zweili <andreas@2li.ch>
2016-12-12 13:29:34 +0100

    add space after the input message
    To have it look a bit better I add a space and a :

--- general/backup/borg_interface/borg_interface.py
+++ general/backup/borg_interface/borg_interface.py
@@ -15,10 +15,10 @@ while chosen_activity != 0:
         if chosen_activity == 1:
             os.system('borg list | less')
         if chosen_activity == 2:
-            archive_name = input("Please enter the archive name ")
+            archive_name = input("Please enter the archive name: ")
             os.system('borg info ::' + archive_name)
         if chosen_activity == 3:
-            archive_name = input("Please enter the archive name")
+            archive_name = input("Please enter the archive name: ")
             mount_point = "/tmp/" + archive_name
             if not os.path.exists(mount_point):
                     os.makedirs(mount_point)
